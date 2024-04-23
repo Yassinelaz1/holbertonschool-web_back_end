66f6ad1159345d2d84821f51e58ca6951f3f50d1
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-04-23 18:11:40 +0100

    Python - Variable Annotations

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -2,10 +2,9 @@
 """ function floor which takes a float n as
 argument and returns the floor of the float"""
 
+import math
+
 
 def floor(n: float) -> float:
     """returns the floor of the float"""
-    if n >= 0:
-        return int(n)  
-    else:
-        return int(n) - 1
+    return math.floor(n)
---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Annotate the below function’s parameters
+and return values with the appropriate types"""
+
+from typing import Iterable, List, Tuple, Sequence
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns a list of tuples"""
+    return [(i, len(i)) for i in lst]
