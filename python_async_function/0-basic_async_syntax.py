f2d4e8f7df419e0c76b9df9d39133c3305a732e0
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-04-25 00:56:40 +0100

    Python - Async

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -9,6 +9,6 @@ import asyncio
 
 async def wait_random(max_delay: int = 10) -> float:
     """The basics of async"""
-    delay=random.randint(0, max_delay)
+    delay = random.randint(0, max_delay)
     await asyncio.sleep(delay)
     return delay
--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,8 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Return the list of delays in ascending order"""
-    delays = await asyncio.gather(*[wait_random(max_delay) for i in range(n)])
+    delays = await wait_random((max_delay) for i in range(n))
     return sorted(delays)
