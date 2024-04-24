6f97106e9761803904cc5ffa623b4ad4eff97f22
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-04-25 00:48:56 +0100

    Python - Async

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Write an asynchronous coroutine that takes in an integer argument
+named wait_random that waits for a random delay between 0 and max_delay
+seconds and eventually returns it."""
+
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """The basics of async"""
+    delay=random.randint(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" execute multiple coroutines at the same time with async"""
+
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ Return the list of delays in ascending order"""
+    delays = await asyncio.gather(*[wait_random(max_delay) for i in range(n)])
+    return sorted(delays)
