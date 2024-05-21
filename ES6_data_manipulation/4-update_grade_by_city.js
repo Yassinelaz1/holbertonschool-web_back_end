2ad7f8a9c300c5010bb4f378a62bc037d038f437
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-05-21 18:32:26 +0100

    4-update_grade_by_city.js

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function getStudentsByLocation(array, city, grad) {
+  return array
+    .filter((i) => i.location === city)
+    .map((student) => {
+      const gradeFilter = grad
+        .filter((i) => i.id === student.id)
+        .map((x) => x.grade)[0];
+      const grade = gradeFilter || 'N/A';
+      return { ...student, grade };
+    });
+}
