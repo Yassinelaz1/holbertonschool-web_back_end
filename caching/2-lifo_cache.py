358e9ee6119c17d42f5525994b29b2fc9bd4c3b3
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-09-25 17:30:55 +0100

    update

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""LIFO Caching"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+from base_caching import BaseCaching
+
+class LIFOCache(BaseCaching):
+    def __init__(self):
+        """Initialize the class and call the parent class init"""
+        super().__init__()
+        self.last_key = None  # To keep track of the last added key
+
+    def put(self, key, item):
+        """Assign item to the dictionary with LIFO eviction policy"""
+        if key is None or item is None:
+            return
+
+        # If the number of items exceeds the limit, remove the last added item (LIFO)
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+            if self.last_key is not None:
+                print(f"DISCARD: {self.last_key}")
+                del self.cache_data[self.last_key]
+
+        # Add new item and update the last key
+        self.cache_data[key] = item
+        self.last_key = key
+
+    def get(self, key):
+        """Return the value in cache linked to key, or None if key is not present"""
+        return self.cache_data.get(key, None)
---
+++ caching/4-mru_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""MRU Caching"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+class MRUCache(BaseCaching):
+    def __init__(self):
+        """Initialize the class and call the parent class init"""
+        super().__init__()
+        self.mru_key = None  # Track the most recently used key
+
+    def put(self, key, item):
+        """Assign item to the dictionary with MRU eviction policy"""
+        if key is None or item is None:
+            return
+
+        # If key already exists, update it but do not count it as a new put
+        if key in self.cache_data:
+            self.cache_data[key] = item
+        else:
+            # If cache is full, discard the most recently used item
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                if self.mru_key is not None:
+                    print(f"DISCARD: {self.mru_key}")
+                    del self.cache_data[self.mru_key]
+
+            # Add new item to cache
+            self.cache_data[key] = item
+
+        # Update the most recently used key
+        self.mru_key = key
+
+    def get(self, key):
+        """Return the value in cache linked to key, or None if key is not present"""
+        if key is None or key not in self.cache_data:
+            return None
+
+        # Update the most recently used key
+        self.mru_key = key
+        return self.cache_data[key]
