f0f980e5bc7a4b9a130c11c80f4da27fe30b7c93
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-09-25 18:42:05 +0100

    update

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """LIFO Caching"""
 
-from base_caching import BaseCaching
+
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -9,25 +9,22 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """Initialize the class and call the parent class init"""
         super().__init__()
-        self.last_key = None  # To keep track of the last added key
+        self.last_key = None
 
     def put(self, key, item):
         """Assign item to the dictionary with LIFO eviction policy"""
+        b = BaseCaching.MAX_ITEMS 
         if key is None or item is None:
             return
-
-        # If the number of items exceeds the limit, remove the last added item
-        # (LIFO)
         if len(
-                self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                self.cache_data) >= b and key not in self.cache_data:
             if self.last_key is not None:
                 print(f"DISCARD: {self.last_key}")
                 del self.cache_data[self.last_key]
-
-        # Add new item and update the last key
         self.cache_data[key] = item
         self.last_key = key
 
     def get(self, key):
-        """Return the value in cache linked to key, or None if key is not present"""
+        """Return the value in cache linked to key,
+        or None if key is not present"""
         return self.cache_data.get(key, None)
