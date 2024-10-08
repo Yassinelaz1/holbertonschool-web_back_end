9f7aed3f85494630ee7397ff76a98cb05090a61a
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-10-08 10:12:58 +0100

    update

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -29,3 +29,35 @@ class BasicAuth(Auth):
     def decode_base64_authorization_header(
             self, base64_authorization_header: str) -> str:
         """returns the decoded value of a Base64 encoded string"""
+        if base64_authorization_header is None:
+            return None, None
+
+        if not isinstance(base64_authorization_header, str):
+            return None, None
+
+        if ':' not in base64_authorization_header:
+            return None, None
+
+        credentials = base64_authorization_header.split(':', 1)
+
+        return credentials[0], credentials[1]
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """that returns the user email and password from the Base64 decoded value."""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        try:
+            found_users = User.search({'email': user_email})
+        except Exception:
+            return None
+
+        for user in found_users:
+            if user.is_valid_password(user_pwd):
+                return user
+
+        return None
