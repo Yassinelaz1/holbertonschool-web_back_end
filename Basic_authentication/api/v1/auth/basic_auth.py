737a94524e3ad253316afd71335cbfebeff49170
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-10-08 10:26:07 +0100

    update

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -40,29 +40,19 @@ class BasicAuth(Auth):
         except Exception:
             return None
 
-    def user_object_from_credentials(self,
-                                     user_email: str,
-                                     user_pwd: str) -> TypeVar('User'):
-        """
-        Returns the User instance based on his
-        email and password
-        """
-        if user_email is None or not isinstance(user_email, str):
-            return None
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str) -> (str, str):
+        """Extracts user email and password from Base64 decoded string."""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
 
-        if user_pwd is None or not isinstance(user_pwd, str):
-            return None
-
-        try:
-            found_users = User.search({'email': user_email})
-        except Exception:
-            return None
-
-        for user in found_users:
-            if user.is_valid_password(user_pwd):
-                return user
-
-        return None
+        # Split the string at the first occurrence of ':'
+        user_email, password = decoded_base64_authorization_header.split(':', 1)
+        return user_email, password
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ overloads Auth and retrieves the User instance for a request """
