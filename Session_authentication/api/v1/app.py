b51d008d9bc34408a0ff90d42d93493c459fdeb7
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-10-09 14:27:52 +0100

    update

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -14,26 +14,26 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 AUTH_TYPE = getenv("AUTH_TYPE")
-if AUTH_TYPE == 'auth':
+
+if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif AUTH_TYPE == 'basic_auth':
+elif AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif AUTH_TYPE == "session_auth":
-    from api.v1.auth.session_auth import SessionAuth
-    auth = SessionAuth()
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
-    """ error handler for (unauthorized) 401 status code """
+def unauthorized_error(error) -> str:
+    """ Unauthorized handler
+    """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error) -> str:
-    """ error handler for (forbidden) 403 status code """
+def forbidden_error(error) -> str:
+    """ Forbidden handler
+    """
     return jsonify({"error": "Forbidden"}), 403
 
 
@@ -46,21 +46,24 @@ def not_found(error) -> str:
 
 @app.before_request
 def before_request() -> str:
-    """ method to handler before request """
+    """ Before Request Handler
+    Requests Validation
+    """
     if auth is None:
         return
+
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/',
-                      '/api/v1/auth_session/login/']
+                      '/api/v1/forbidden/']
+
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if not auth.authorization_header(request)\
-       and not auth.session_cookie(request):
+
+    if auth.authorization_header(request) is None:
         abort(401)
+
     if auth.current_user(request) is None:
         abort(403)
-    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
