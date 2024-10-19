07e76b8dd28040284d1deef14f6d20627c42c4fa
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-10-19 12:49:48 +0100

    update

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 """Define the base class for declarative models"""
 Base = declarative_base()
