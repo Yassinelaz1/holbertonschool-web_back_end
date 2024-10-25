9b905d5281720853a372032379132fa5891e3337
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-10-25 17:52:43 +0100

    README.md

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -13,13 +13,12 @@ class TestGithubOrgClient(unittest.TestCase):
     """ test """
     @parameterized.expand([("google"),("abc"),])
     @patch("client.get_json", return_value={"payload": True})
-    def test_org(self, org_name,mock_get_json):
+    def test_org(self, org_name, mock_get_json):
         """ test that GithubOrgClient.org returns the correct value """
         
         mock_get_json.return_value = {org_name: 'true'}
         client = GithubOrgClient(org_name)
         result = client.org()
-        mock_get_json.assert_called_once_with(
-            f"https://api.github.com/orgs/{org_name}"
-        )
+        url = f'https://api.github.com/orgs/{org_name}'
+        mock_get_json.assert_called_once_with(url)
         self.assertEqual(result, {org_name: 'true'} )
