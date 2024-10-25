327b432b9401897591613bff16f9c70bc538353b
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-10-25 17:08:12 +0100

    README.md

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -31,3 +31,17 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as error:
             access_nested_map(nested_map, path)
         self.assertEqual(error.exception.args[0], path[-1])
+
+class TestGetJson(unittest.TestCase):
+    """ test """
+    """ Mock HTTP calls"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('test_utils.get_json')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """method to test that utils.get_json returns the expected result."""
+        mock_get.return_value = test_payload
+        result = get_json(test_url)
+        self.assertEqual(result, test_payload)
