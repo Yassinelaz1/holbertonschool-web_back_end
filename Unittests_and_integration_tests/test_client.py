bc57b21e688f97e056be31273aee027db727bdc8
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-10-25 18:07:25 +0100

    README.md

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -11,10 +11,23 @@ from utils import access_nested_map, get_json, memoize
 
 class TestGithubOrgClient(unittest.TestCase):
     """ test """
-    @parameterized.expand([("google"),("abc"),])
-    @patch("client.get_json", return_value={"payload": True})
-    def test_access_nested_map_exception(self, nested_map, path):
-        """ 5. Mocking a property"""
-        with self.assertRaises(KeyError) as error:
-            access_nested_map(nested_map, path)
-        self.assertEqual(error.exception.args[0], path[-1])
+    @parameterized.expand([("google"), ("abc"),])
+    @patch("client.get_json", value={"payload": True})
+    def test_org(self, org_name, mock_get_json):
+        """ test that GithubOrgClient.org returns the correct value """
+        test_client = GithubOrgClient(org_name)
+        test_return = test_client.org
+        self.assertEqual(test_return, mock_get_json.value)
+        mock_get_json.assert_called_once
+
+    def test_public_repos_url(self, mock_org):
+        """test_public_repos_url method"""
+
+        test_url = f'https://api.github.com/orgs/test/repo'
+
+        mock_org.return_value = {'repos_url': test_url}
+
+        client = GithubOrgClient('test')
+        result = client._public_repos_url()
+
+        self.assertEqual('https://api.github.com/orgs/test/repo', test_url)
