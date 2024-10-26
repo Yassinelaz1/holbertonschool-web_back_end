5b353f912a384cc9594ccdfc92e20ae6fbfeb785
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-10-26 16:17:57 +0100

    update

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -14,8 +14,8 @@ class Config(object):
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app.config.from_object(Config)
 
+app.config.from_object(Config)
 
 @app.route('/')
 def root():
@@ -33,6 +33,7 @@ def get_locale():
         return lang
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+babel = Babel(app, locale_selector=get_locale)
 
 if __name__ == "__main__":
     app.run()
