20de48bb2b73bfe4036c139f0587c447905d9374
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-10-26 15:58:03 +0100

    update

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 "Basic Flask app"
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
 app = Flask(__name__)
 babel = Babel(app)
+
 
 class Config:
     "Basic Babel setup"
@@ -16,18 +17,18 @@ class Config:
 
 app.config.from_object(Config)
 
+
 @app.route('/')
 def root():
     """ render 2-index.html """
     return render_template("2-index.html")
 
+
 @babel.localeselector
 def get_locale():
     """  to determine the best match with our supported languages. """
-    lang = request.args.get('lang')
-    if lang in app.config['LANGUAGES']:
-        return lang
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 if __name__ == "__main__":
     app.run()
