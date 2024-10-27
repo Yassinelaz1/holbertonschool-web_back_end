05aa224da362f701091c244cfee2cd233eeaca39
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-10-27 20:20:17 +0100

    update

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,41 +1,41 @@
 #!/usr/bin/env python3
-"Basic Flask app"
+"""
+Flask application
+"""
+
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
+from flask_babel import Babel
 
 
 app = Flask(__name__)
-babel = Babel(app)
 
 
-class Config(object):
+class Config:
     """ config class """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app.config.from_object(Config)
-
-
-@app.route('/')
-def root():
-    """ render 4-index.html """
-    return render_template("4-index.html")
-
-
-@babel.localeselector
 def get_locale():
     """
     to Determine the best match with our supported languages.
     """
-    local_Lang = request.args.get('locale')
-    supportLang = app.config['LANGUAGES']
-    if local_Lang in supportLang:
-        return local_Lang
-    else:
-        return request.accept_languages.best_match(app.config['LANGUAGES'])
+    lang = request.args.get('lang')
+    if lang in app.config['LANGUAGES']:
+        return lang
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-if __name__ == "__main__":
+babel = Babel(app, locale_selector=get_locale)
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """Renders 4-index.html"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
     app.run()
