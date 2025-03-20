e42a4ba4fe1ac0595e8ff07546d3baf17a6ab37e
Yassinelaz1 <yassinelazhari524@gmail.com>
2025-03-20 09:14:36 +0100

    update

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
     """Cache class for storing data in Redis."""
+
     def __init__(self):
         """Initialize the Redis client and flush the database."""
         self._redis = redis.Redis()
@@ -23,3 +24,26 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable] = None) -> Union[str,
+                                                    bytes,
+                                                    int,
+                                                    float,
+                                                    None]:
+        """
+        Retrieve data from Redis and optionally apply a conversion function."""
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        return fn(data) if fn else data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieve a string from Redis."""
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """Retrieve an integer from Redis."""
+        return self.get(key, fn=int)
