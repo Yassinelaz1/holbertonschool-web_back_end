4a09ee79c5b0d5a099868d0de9eb5d5a60450754
Yassinelaz1 <yassinelazhari524@gmail.com>
2025-03-23 17:47:46 +0100

    update

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -7,7 +7,7 @@ BEGIN
 
     SELECT AVG(score) INTO avg_score
     FROM corrections
-    WHERE user_id = user_id;
+    WHERE corrections.user_id = user_id;
 
     UPDATE users
     SET average_score = avg_score
